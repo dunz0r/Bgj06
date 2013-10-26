6b948101f63748deddd7adf5461a8714f1cdddde
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 14:18:21 +0200

    / ‘main.cpp’

--- main.cpp
+++ main.cpp
@@ -1,4 +1,14 @@
 // A Game for bacongamejam06
 //
 
-#include <allegro.h>
+#include <stdio.h>
+
+// Allegro
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_font.h>
+#include <allegro5/allegro_ttf.h>
+#include <allegro5/allegro_audio.h>
+#include <allegro5/allegro_acodec.h>
+#include <allegro5/allegro_primitives.h>
+
