ff1f5292646af505150daaa885ad3784559336ba
Johannes Fornaeus <johannes.fornaeus@gmail.com>
2013-10-26 14:50:28 +0200

    / ‘main2.cpp’

--- main2.cpp
+++ main2.cpp
@@ -27,7 +27,10 @@ class cPlayer
 	void run(ALLEGRO_KEYBOARD_STATE *key)
 	{
 		//vspeed += gravity;
-		if(al_key_down(key,ALLEGRO_KEY_RIGHT));
+		if(al_key_down(key,ALLEGRO_KEY_SPACE))
+		{
+			
+		}
 	}
 };
 
