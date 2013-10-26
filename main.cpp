88d1862c74199821d7933605f88194498f4cb663
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 14:22:47 +0200

    / ‘main.cpp’

--- main.cpp
+++ main.cpp
@@ -12,3 +12,16 @@
 #include <allegro5/allegro_acodec.h>
 #include <allegro5/allegro_primitives.h>
 
+int main(int argc, const char *argv[]){
+	// Initialize Allegro
+	al_init();
+	al_init_ttf_addon();
+	al_install_keyboard();
+	al_install_mouse();
+	al_install_audio;
+	al_init_acodec_addon();
+	al_reserve_samples();
+	ALLEGRO_KEYBOARD_STATE key;
+	ALLEGRO_MOUSE_STATE mouse;
+	return 0;
+}
