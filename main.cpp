14564bee788a6bcb710117fa6c7abcb340a68f2b
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 16:04:44 +0200

    / ‘game’ / ‘main.cpp’ / ‘main.o’

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ class cRainbow
 	void draw()
 	{
 		for (int i = 0; i < 7; i++) {
-			al_draw_ellipse(cx, cy+(i*4), rx, ry, color[i], thickness);
+			al_draw_ellipse(cx, cy+(i*6), rx, ry, color[i], thickness);
 		}
 	}
 };
