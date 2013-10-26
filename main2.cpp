c1aa4aabe5b92edfad8d2460c2b5a2756dae8891
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 16:40:26 +0200

    / ‘game’ / ‘main2.cpp’ / ‘main2.o’

--- main2.cpp
+++ main2.cpp
@@ -226,7 +226,7 @@ int main(int argc, char **argv )
 	oLevel.init();
 
 	cRainbow oRainbow;
-	oRainbow.create((oLevel.blockamount/2)*32, 30, 220);
+	oRainbow.create((oLevel.blockamount/2)*32, ((oLevel.blockamount)*32)/2, 220);
 
 	////
 	al_clear_to_color(al_map_rgb(0,0,0));
