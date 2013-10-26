5e3b6384fcadb7afc3467d3dd7c14d69440c4875
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 15:24:32 +0200

    / ‘main.cpp’

--- main.cpp
+++ main.cpp
@@ -45,7 +45,9 @@ class cRainbow
 			al_draw_arc(cx, cy, radius, startTheta, deltaTheta, color[i], thickness);
 		}
 		*/
-		al_draw_arc(1, 1, 60, 40, 40, al_map_rgb(255,255,0), thickness);
+		//al_draw_arc(float cx, float cy, float r, float start_theta,
+		//   float delta_theta, ALLEGRO_COLOR color, float thickness)
+		al_draw_arc(1, 1, 600, 40, 40, al_map_rgb(255,255,0), thickness);
 	}
 };
 int main(int argc, char **argv )
