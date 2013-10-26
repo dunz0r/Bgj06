1fd78c8d54a9c4c9937b50c240b00fef970dca4d
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 15:13:40 +0200

    / ‘main.cpp’

--- main.cpp
+++ main.cpp
@@ -39,7 +39,7 @@ class cRainbow
 
 	void draw()
 	{
-		for (i = 0; i < 7; i++) {
+		for (int i = 0; i < 7; i++) {
 			cx +=3;
 			al_draw_arc(cx, cy, radius, color[i], startTheta, deltaTheta, thickness);
 		}
