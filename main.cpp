74398f1094efc88ef0dd1ac8cbf98713e94bf0e2
Gabriel Fornaeus <gf@hax0r.se>
2013-10-26 16:04:09 +0200

    / ‘game’ / ‘main.cpp’ / ‘main.o’

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@ class cRainbow
 	public:
 		float cx,cy,rx,ry,thickness;
 		ALLEGRO_COLOR color[7] = { al_map_rgb(0,0,0) };
-	void create(float cy, float cx)
+	void create()
 	{
 		color[0] = al_map_rgb(255,0,0);
 		color[1] = al_map_rgb(255,127,0);
@@ -80,7 +80,7 @@ int main(int argc, char **argv )
 	al_reserve_samples(1000);
 
 	cRainbow oRainbow;
-	oRainbow.create(200,500);
+	oRainbow.create();
 	//RUN
 	while(1<2)
 	{
